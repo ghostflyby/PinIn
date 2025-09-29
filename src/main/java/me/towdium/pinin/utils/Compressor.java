c62491738e76d7a884b5c24f6cf23ecde157b726
ghostflyby <ghostflyby+git@outlook.com>
2025-09-29 10:19:18 +0800

    refactor: replace CharList with StringBuilder for improved performance

--- src/main/java/me/towdium/pinin/utils/Compressor.java
+++ src/main/java/me/towdium/pinin/utils/Compressor.java
@@ -1,12 +1,10 @@
 package me.towdium.pinin.utils;
 
-import it.unimi.dsi.fastutil.chars.CharArrayList;
-import it.unimi.dsi.fastutil.chars.CharList;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 
 public class Compressor implements Accelerator.Provider {
-    CharList chars = new CharArrayList();
+    StringBuilder chars = new StringBuilder();
     IntList strs = new IntArrayList();
 
     public IntList offsets() {
@@ -14,23 +12,22 @@ public class Compressor implements Accelerator.Provider {
     }
 
     public int put(String s) {
-        strs.add(chars.size());
-        for (int i = 0; i < s.length(); i++)
-            chars.add(s.charAt(i));
-        chars.add('\0');
+        strs.add(chars.length());
+        chars.append(s);
+        chars.append('\0');
         return strs.getInt(strs.size() - 1);
     }
 
     @Override
     public boolean end(int i) {
-        return chars.getChar(i) == '\0';
+        return chars.charAt(i) == '\0';
     }
 
     @Override
     public int get(int i) {
-        char first = chars.getChar(i);
-        if (Character.isHighSurrogate(first) && i + 1 < chars.size()) {
-            char second = chars.getChar(i + 1);
+        char first = chars.charAt(i);
+        if (Character.isHighSurrogate(first) && i + 1 < chars.length()) {
+            char second = chars.charAt(i + 1);
             if (Character.isLowSurrogate(second)) return Character.toCodePoint(first, second);
         }
         return first;
