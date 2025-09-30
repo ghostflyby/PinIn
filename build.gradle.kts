7e028167a1c649081dfe6291e9d3e511bdd15b25
ghostflyby <ghostflyby+git@outlook.com>
2025-09-30 12:26:34 +0800

    build: minify shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,36 +11,8 @@ repositories {
     mavenCentral()
 }
 
-tasks.build {
-    dependsOn(tasks.shadowJar)
-}
-
 tasks.shadowJar {
-    exclude("it/unimi/dsi/fastutil/shorts/**")
-    exclude("it/unimi/dsi/fastutil/longs/**")
-    exclude("it/unimi/dsi/fastutil/floats/**")
-    exclude("it/unimi/dsi/fastutil/doubles/**")
-    exclude("it/unimi/dsi/fastutil/bytes/**")
-    exclude("it/unimi/dsi/fastutil/booleans/**")
-    exclude("it/unimi/dsi/fastutil/ints/*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Short*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Reference*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Long*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Int*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Float*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Double*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Char*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Byte*Map*")
-    exclude("it/unimi/dsi/fastutil/objects/*2Boolean*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Short*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Reference*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Long*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Int*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Float*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Double*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Char*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Byte*Map*")
-    exclude("it/unimi/dsi/fastutil/chars/*2Boolean*Map*")
+    minimize()
     relocate("it.unimi.dsi.fastutil", "me.towdium.pinin.fastutil")
 }
 
